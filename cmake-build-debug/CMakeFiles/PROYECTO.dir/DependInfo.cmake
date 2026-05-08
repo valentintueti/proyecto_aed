d3829fc3caa23a30dde4b5468cada6f4e35977a4
Valentín Tuesta <valentin.tuesta@utec.edu.pe>
2026-05-08 17:39:22 -0500

    Complejidades en el README

---
+++ cmake-build-debug/CMakeFiles/PROYECTO.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/valma/Downloads/aed/PROYECTO/formula.cpp" "CMakeFiles/PROYECTO.dir/formula.cpp.obj" "gcc" "CMakeFiles/PROYECTO.dir/formula.cpp.obj.d"
+  "C:/Users/valma/Downloads/aed/PROYECTO/main.cpp" "CMakeFiles/PROYECTO.dir/main.cpp.obj" "gcc" "CMakeFiles/PROYECTO.dir/main.cpp.obj.d"
+  "C:/Users/valma/Downloads/aed/PROYECTO/spreadsheet_ui.cpp" "CMakeFiles/PROYECTO.dir/spreadsheet_ui.cpp.obj" "gcc" "CMakeFiles/PROYECTO.dir/spreadsheet_ui.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PROYECTO.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROYECTO.dir/formula.cpp.obj"
+  "CMakeFiles/PROYECTO.dir/formula.cpp.obj.d"
+  "CMakeFiles/PROYECTO.dir/main.cpp.obj"
+  "CMakeFiles/PROYECTO.dir/main.cpp.obj.d"
+  "CMakeFiles/PROYECTO.dir/spreadsheet_ui.cpp.obj"
+  "CMakeFiles/PROYECTO.dir/spreadsheet_ui.cpp.obj.d"
+  "PROYECTO.exe"
+  "PROYECTO.exe.manifest"
+  "PROYECTO.pdb"
+  "libPROYECTO.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PROYECTO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
